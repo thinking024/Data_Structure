2572f131b7b4343dcfb32026db32b768e75ab351
YiyuanZhu <2643811975@qq.com>
2021-11-05 22:09:14 +0800

    finish dfs for matrix graph

--- graph/AMGraph.cpp
+++ graph/AMGraph.cpp
@@ -11,10 +11,12 @@ typedef int Arc;
 
 typedef struct AMGraph
 {
-    VerTex vexs[MVNUM]; // 存储顶点的数组
+    VerTex vexs[MVNUM];     // 存储顶点的数组
     Arc arcs[MVNUM][MVNUM]; // 存储边的数组
     int vexNum, arcNum;
 } AMGraph;
+
+int visited[MVNUM] = {0};
 
 // 用于定位顶点在顶点数组中的下标
 int getIndex(VerTex vexs[], VerTex vex)
@@ -26,7 +28,7 @@ int getIndex(VerTex vexs[], VerTex vex)
             return i;
         }
     }
-    return -1;   
+    return -1;
 }
 
 void create(AMGraph &graph)
@@ -36,11 +38,11 @@ void create(AMGraph &graph)
     {
         cin >> graph.vexs[i];
     }
-    for (size_t i = 0; i < graph.arcNum; i++)
+    for (size_t i = 0; i < graph.vexNum; i++)
     {
-        for (size_t j = 0; j < graph.arcNum; i++)
+        for (size_t j = 0; j < graph.vexNum; j++)
         {
-            graph.arcs[i][j] = MAXINT; // 将邻接矩阵的权值置为无穷大 
+            graph.arcs[i][j] = MAXINT; // 将邻接矩阵的权值置为无穷大
         }
     }
     for (size_t k = 0; k < graph.arcNum; k++)
@@ -55,7 +57,37 @@ void create(AMGraph &graph)
     }
 }
 
+// 从vex顶点开始，dfs遍历连通图
+void DFS(AMGraph graph, int vex)
+{
+    cout << graph.vexs[vex] << " ";
+    visited[vex] = 1;
+    for (size_t i = 0; i < graph.vexNum; i++)
+    {
+        // 访问与之相连且未被访问过的结点
+        if (graph.arcs[vex][i] != 0 && visited[i] == 0)
+        {
+            DFS(graph, i);
+        }
+    }
+}
+
+// 从vex顶点开始，dfs遍历非连通图
+void DFSUnconnected(AMGraph graph)
+{
+    for (size_t i = 0; i < graph.vexNum; i++)
+    {
+        if (visited[i] == 0)
+        {
+            DFS(graph, i);
+        }   
+    }
+}
+
 int main()
 {
+    AMGraph graph;
+    create(graph);
+    DFSUnconnected(graph);
     return 0;
 }
